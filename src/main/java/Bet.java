30e6c13c95f47d47dbbb8147824b7092b508e7e9
wajahat <wajahat.ahmed245@gmail.com>
2019-08-17 19:56:28 +0500

    col int

---
+++ src/main/java/Bet.java
@@ -0,0 +1,3 @@
+public interface Bet {
+    public static void printMe(){};
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -12,16 +12,86 @@ import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class test {
+import java.util.stream.Collectors;
 
-    public static void main(String[] args) {
+import java.io.*;
+
+
+public class test implements Bet {
+
+    public static class SortMapByValue {
+
+        public boolean ASC = true;
+        public boolean DESC = false;
+        private Map<String, Long> unsortMap = new HashMap<String, Long>();
+
+
+        public Map<String, Long> getUnsortMap() {
+            return unsortMap;
+        }
+
+        public void insertKeyValue(String key, Long value) {
+            unsortMap.put(key, value);
+
+
+        }
+
+        public Map<String, Long> sortByComparator(Map<String, Long> unsortMap, final boolean order) {
+
+            List<Map.Entry<String, Long>> list = new LinkedList<Map.Entry<String, Long>>(unsortMap.entrySet());
+
+
+            Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
+                public int compare(Map.Entry<String, Long> o1,
+                                   Map.Entry<String, Long> o2) {
+                    if (order) {
+                        return o1.getValue().compareTo(o2.getValue());
+                    } else {
+                        return o2.getValue().compareTo(o1.getValue());
+
+                    }
+                }
+            });
+
+
+            Map<String, Long> sortedMap = new LinkedHashMap<String, Long>();
+            for (Map.Entry<String, Long> entry : list) {
+                sortedMap.put(entry.getKey(), entry.getValue());
+            }
+
+            return sortedMap;
+        }
+
+        public String getMeModeKey() {
+            Map<String, Long> sortedMapDesc = sortByComparator(unsortMap, DESC);
+            return printMap(sortedMapDesc);
+        }
+
+        public String printMap(Map<String, Long> map) {
+            String firstKey = map.keySet().iterator().next();
+            return firstKey;
+        }
+
+        public <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+            return map.entrySet()
+                    .stream()
+                    .filter(entry -> Objects.equals(entry.getValue(), value))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toSet());
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
 
         SortMapByValue hashmap = new SortMapByValue();
 
-        hashmap.insertKeyValue("hello12", Long.valueOf(12));
-        hashmap.insertKeyValue("hello11", Long.valueOf(13));
-        hashmap.insertKeyValue("hello112", Long.valueOf(16));
-        hashmap.insertKeyValue("hello1", Long.valueOf(16));
+        hashmap.insertKeyValue("hello12", Long.valueOf(1));
+        hashmap.insertKeyValue("hello11", Long.valueOf(1));
+        hashmap.insertKeyValue("hello112", Long.valueOf(1));
+        hashmap.insertKeyValue("hello1", Long.valueOf(1));
 
 
         System.out.println(hashmap.getMeModeKey());
@@ -42,11 +112,52 @@ public class test {
 //        System.out.println(getKeysByValue(hashMap_non, Long.valueOf(3)).toString());
 //        System.out.println(hashmap.getUnsortMap().getClass().getSimpleName());
         System.out.println(hashMap_non.getClass().getSimpleName());
-hashmap.getUnsortMap();
+        hashmap.getUnsortMap();
 
-hashmap.getKeysByValue(hashmap.getUnsortMap(),hashmap.getUnsortMap().get(hashmap.getMeModeKey()));
-        System.out.println(hashmap.getKeysByValue(hashmap.getUnsortMap(),hashmap.getUnsortMap().get(hashmap.getMeModeKey())));
+        hashmap.getKeysByValue(hashmap.getUnsortMap(), hashmap.getUnsortMap().get(hashmap.getMeModeKey()));
+        System.out.println("this is " + hashmap.getUnsortMap().get(hashmap.getMeModeKey()));
 
+
+        if (hashmap.getUnsortMap().get(hashmap.getMeModeKey()) == Long.valueOf(1)) {
+
+            System.out.println("no mode");
+        } else {
+            System.out.println("this is in if " + hashmap.getUnsortMap().get(hashmap.getMeModeKey()));
+        }
+
+//        System.out.println(hashmap.getKeysByValue(hashmap.getUnsortMap(), hashmap.getUnsortMap().get(hashmap.getMeModeKey())).toString());
+
+        StringBuilder text = new StringBuilder("javatutorialhq.com");
+        text.replace(0, text.length(), "hello12");
+//        System.out.println(text.toString().getClass().getSimpleName());
+        System.out.println(hashmap.getKeysByValue(hashmap.getUnsortMap(), hashmap.getUnsortMap().get(hashmap.getMeModeKey())).toString().replace("[", "").replace("]", ""));
+
+        File file = new File("/home/wajahat/Documents/eventlistener-File/filename.txt");
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("this is my file");
+        pw.close();
+
+        String num = "132.42";
+        StringBuilder sb = new StringBuilder(num);
+        ReplaceString rs1 = new ReplaceString();
+        try {
+            System.out.println(Double.parseDouble(num));
+//            sb.delete(0,sb.length());
+//            sb.append(num.replace(".","point"));
+            sb.insert(0, "Col");
+
+            System.out.println(sb);
+
+            System.out.println(rs1.validationsCol(sb));
+
+        } catch (Exception e) {
+            System.out.println(rs1.validationsCol(sb));
+        }
     }
 
 
